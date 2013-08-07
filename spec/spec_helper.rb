97d9cde5e1bc24fd58242f78357ed77c30d9e7c5
Daniel Woelfel <dwwoelfel@gmail.com>
2013-08-07 09:42:04 -0700

    no profiler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,17 @@
 require 'rspec'
-require 'ruby-prof'
+#require 'ruby-prof'
 
-RubyProf.measure_mode = RubyProf::MEMORY
+#RubyProf.measure_mode = RubyProf::MEMORY
 
 RSpec.configure do |config|
 
   config.before(:each) do
-    RubyProf.start
+    #RubyProf.start
+    p 'hi'
   end
 
   config.after(:each) do
-    RubyProf::FlatPrinter.new(RubyProf.stop).print(STDOUT)
+    #RubyProf::FlatPrinter.new(RubyProf.stop).print(STDOUT)
+    p 'bye'
   end
 end
