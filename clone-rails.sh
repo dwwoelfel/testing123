d5625ff67f13c01f55a0029f223421daa5e761ec
Daniel Woelfel <dwwoelfel@gmail.com>
2013-08-30 17:38:47 -0700

    eh?

--- clone-rails.sh
+++ clone-rails.sh
@@ -1,6 +1,6 @@
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 while true
 do
-    git clone git@github.com:rails/rails.git
+    git clone https://github.com/rails/rails.git
     rm -r rails
 done
